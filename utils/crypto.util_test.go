fb7aece008836ddd2ce6cbf4ac66bd5b1d569bbc
Panithi Nakkhruea <panithi.nakkhruea@pm.me>
2022-04-16 11:14:53 +0700

    Add unt test for hash string

--- utils/crypto.util_test.go
+++ utils/crypto.util_test.go
@@ -1,6 +1,9 @@
 package utils_test
 
 import (
+	"crypto"
+	_ "crypto/sha256"
+	"fmt"
 	"testing"
 
 	. "github.com/argonlab-io/bucharest/utils"
@@ -24,4 +27,10 @@ func TestRandomByteSucess(t *testing.T) {
 
 	b = NewEncoder(nil).Random(0).Bytes()
 	assert.Empty(t, b)
+}
+
+func TestHashing(t *testing.T) {
+	foobar := "foobar"
+	hash := NewEncoder(&foobar).Hash(crypto.SHA256)
+	assert.Equal(t, fmt.Sprintf("%x", hash), "c3ab8ff13720e8ad9047dd39466b3c8974e592c2fa383d4a3960714caef0c4f2")
 }
