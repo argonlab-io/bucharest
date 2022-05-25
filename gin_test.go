1b2811c5f5c43cfc9b391ed0dfe373a2fd9cee14
Panithi Nakkhruea <panithi.nakkhruea@pm.me>
2022-05-26 00:32:29 +0700

    Create unit test TestGetterAndSetter

--- gin_test.go
+++ gin_test.go
@@ -512,7 +512,6 @@ func TestHandlerControlAbortWithStatusJSON(t *testing.T) {
 		res, err = client.Do(req)
 
 		if res != nil {
-
 			assert.NoError(t, err)
 			assert.Equal(t, res.StatusCode, http.StatusInternalServerError)
 			j := make(map[string]interface{}, 0)
@@ -521,6 +520,110 @@ func TestHandlerControlAbortWithStatusJSON(t *testing.T) {
 			assert.Equal(t, j["message"].(string), "foobar")
 		}
 
+		return err == nil
+	}
+
+	utils.RunUntil(fn, time.Second*4)
+	assert.NoError(t, err)
+	assert.NotNil(t, res)
+}
+
+func TestGetterAndSetter(t *testing.T) {
+	ctx := NewContextWithOptions(nil)
+	assert.NotNil(t, ctx)
+
+	var path string
+
+	handler := func(ctx HTTPContext) HTTPError {
+		bar, ok := ctx.Get("foo")
+		assert.Equal(t, bar, "bar")
+		assert.True(t, ok)
+
+		bar, ok = ctx.MustGet("foo").(string)
+		assert.Equal(t, bar, "bar")
+		assert.True(t, ok)
+
+		bar = ctx.GetString("foo")
+		assert.Equal(t, bar, "bar")
+
+		getInt := ctx.GetInt("int")
+		assert.Equal(t, getInt, 2046)
+
+		getInt64 := ctx.GetInt64("int64")
+		assert.Equal(t, getInt64, int64(2046))
+
+		getUint := ctx.GetUint("uint")
+		assert.Equal(t, getUint, uint(2046))
+
+		getUint64 := ctx.GetUint64("uint64")
+		assert.Equal(t, getUint64, uint64(2046))
+
+		getFloat64 := ctx.GetFloat64("float64")
+		assert.Equal(t, getFloat64, float64(3.14))
+
+		getBool := ctx.GetBool("bool")
+		assert.True(t, getBool)
+
+		getTime := ctx.GetTime("time")
+		assert.Equal(t, getTime, time.Time{})
+
+		getDuration := ctx.GetDuration("duration")
+		assert.Equal(t, getDuration, time.Second)
+
+		getStringSlice := ctx.GetStringSlice("ss")
+		assert.Len(t, getStringSlice, 2)
+		assert.Equal(t, getStringSlice[0], "foobar")
+		assert.Equal(t, getStringSlice[1], "fozbaz")
+
+		getStringMap := ctx.GetStringMap("sm")
+		assert.Len(t, getStringMap, 2)
+		assert.Equal(t, getStringMap["one"], 1)
+		assert.Equal(t, getStringMap["true"], true)
+
+		getStringMapString := ctx.GetStringMapString("sms")
+		assert.Len(t, getStringMapString, 2)
+		assert.Equal(t, getStringMapString["foo"], "bar")
+		assert.Equal(t, getStringMapString["foz"], "baz")
+
+		getStringMapStringSlice := ctx.GetStringMapStringSlice("smss")
+		assert.Len(t, getStringMapStringSlice, 2)
+		assert.Equal(t, getStringMapStringSlice["foo"][0], "bar")
+		assert.Equal(t, getStringMapStringSlice["foz"][0], "baz")
+
+		ctx.Status(http.StatusNoContent)
+		return nil
+	}
+	middleware := func(ctx HTTPContext) HTTPError {
+		ctx.Set("foo", "bar")
+		ctx.Set("int", 2046)
+		ctx.Set("int64", int64(2046))
+		ctx.Set("uint", uint(2046))
+		ctx.Set("uint64", uint64(2046))
+		ctx.Set("float64", float64(3.14))
+		ctx.Set("bool", true)
+		ctx.Set("time", time.Time{})
+		ctx.Set("duration", time.Second)
+		ctx.Set("ss", []string{"foobar", "fozbaz"})
+		ctx.Set("sm", map[string]interface{}{"one": 1, "true": true})
+		ctx.Set("sms", map[string]string{"foo": "bar", "foz": "baz"})
+		ctx.Set("smss", map[string][]string{"foo": {"bar"}, "foz": {"baz"}})
+		return nil
+	}
+	ginHandlerFunc := NewGinHandlerFunc(ctx, handler)
+	assert.NotNil(t, ginHandlerFunc)
+
+	var err error
+	path, err = getCallingPath(ginHandlerFunc, NewGinHandlerFunc(ctx, middleware))
+	assert.NoError(t, err)
+
+	var res *http.Response
+	fn := func() bool {
+		client := &http.Client{}
+		req, err := http.NewRequest(http.MethodGet, path, nil)
+		if err != nil {
+			return false
+		}
+		res, err = client.Do(req)
 		return err == nil
 	}
 
