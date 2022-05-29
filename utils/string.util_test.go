d060d737f87a95f7ecd6b42fcc2fbd175a02cb01
Panithi Nakkhruea <panithi.nakkhruea@pm.me>
2022-05-29 20:52:54 +0700

    Add test case for string utils

--- utils/string.util_test.go
+++ utils/string.util_test.go
@@ -10,6 +10,20 @@ import (
 func TestArrayStringContain(t *testing.T) {
 	arr := []string{"foobar"}
 	foobarIndex, containFoobar := ArrayStringContians(arr, "foobar")
-	assert.True(t,containFoobar)
+	assert.True(t, containFoobar)
 	assert.Equal(t, foobarIndex, 0)
+}
+
+func TestArrayStringContainIndex(t *testing.T) {
+	arr := []string{"foobar", "fozbaz"}
+	fozbazIndex, containFozbaz := ArrayStringContians(arr, "fozbaz")
+	assert.True(t, containFozbaz)
+	assert.Equal(t, fozbazIndex, 1)
+}
+
+func TestArrayStringContainNotFound(t *testing.T) {
+	arr := []string{"foobar"}
+	fozbazIndex, containFozbaz := ArrayStringContians(arr, "fozbaz")
+	assert.False(t, containFozbaz, false)
+	assert.Equal(t, fozbazIndex, len(arr))
 }
